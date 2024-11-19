401d04fa0e72e10d2952dde0ba774e75cf6e695c
wtto00 <wtto00@outlook.com>
2024-11-20 02:48:02 +0800

    自动检查最新版本SDK，并创建issue提醒同步

---
+++ check-version.mjs
@@ -0,0 +1,64 @@
+/**
+ * 每天检查DCloud官方发布的最新SDK版本
+ * 如果有最新版本，则新建一个issue
+ */
+import { Octokit } from "@octokit/rest";
+
+const url = "https://nativesupport.dcloud.net.cn/AppDocs/download/android.html";
+
+console.log(`爬取DCloud离线页面: ${url}`);
+
+const res = await fetch(url);
+
+if (!res.ok) {
+  console.error("爬取DCloud离线页面出错了", res);
+  process.exit(1);
+}
+
+const content = await res.text();
+
+const match = content.match(/\d{4}年\d{2}月\d{2}日发布——HBuilderX（\d+\.\d+\.\d+）/);
+
+const latest = match?.[0];
+
+if (!latest) {
+  console.error(content);
+  process.exit(1);
+}
+
+console.log(`查询到最新版本SDK为: ${latest}`);
+
+const octokit = new Octokit();
+
+const issuesRes = await octokit.rest.search.issuesAndPullRequests({
+  q: `${latest}+is:issue`,
+});
+
+if (issuesRes.status !== 200) {
+  console.error("搜索Issue出错了: ", issuesRes);
+  process.exit(1);
+}
+
+const { items } = issuesRes.data;
+
+const issue = items.find((item) => item.title === latest);
+
+if (issue) {
+  console.log(`已存在最新版本: https://github.com/wtto00/uniapp-android-sdk/issues/${issue.id}`);
+  process.exit(0);
+}
+
+const create = await octokit.rest.issues.create({
+  owner: "wtto00",
+  repo: "uniapp-android-sdk",
+  title: latest,
+  body: `A new version of the Android offline SDK is now available!\n\nGo to [download](${url}) and sync the repository.`,
+});
+
+if (create.status !== 200) {
+  console.error("创建Issue失败了: ", create);
+  process.exit(1);
+}
+
+console.log(`创建Issue成功: ${create.url}`);
+process.exit(0);
